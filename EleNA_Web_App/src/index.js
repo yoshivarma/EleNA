2c9944e5119d4ba66fe851c579c71c13bea7f37c
SUSMITAMADINENI <smadineni@umass.edu>
2022-11-27 05:04:24 -0500

    Map components using openstreetmap api

--- EleNA_Web_App/src/index.js
+++ EleNA_Web_App/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MapDisplay from './map_components/MapDisplay';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MapDisplay />
   </React.StrictMode>
 );
 
