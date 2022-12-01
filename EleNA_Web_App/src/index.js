972ec3562886c5a31d5de1cbba66b7a5b317d53a
SUSMITAMADINENI <smadineni@umass.edu>
2022-12-01 14:57:08 -0500

    Updated App and Index files according to MainInterface

--- EleNA_Web_App/src/index.js
+++ EleNA_Web_App/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import MapDisplay from './map_components/MapDisplay';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MapDisplay />
+    <App />
   </React.StrictMode>
 );
 
