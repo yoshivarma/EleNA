f531288a213dc7eefa9694b4cfa270fcce3d3518
SUSMITAMADINENI <smadineni@umass.edu>
2022-12-16 18:05:31 -0500

    Added selenium testing when maximum elevation gain is selected as input

--- EleNA_Web_App/src/Left_Interface/test/InputTest.py
+++ EleNA_Web_App/src/Left_Interface/test/InputTest.py
@@ -38,12 +38,53 @@ distance = driver.find_element("name", "fee_distance")
 distance_value = distance.get_attribute("value")
 if distance_value == '6116.204':
     print("Obtained distance value is correct")
-#
-# #find elevation statistics and verify the calculated values
+    
+# find elevation statistics and verify the calculated values
 elevation = driver.find_element("name", "fee_elevation")
 elevation_value = elevation.get_attribute("value")
 if elevation_value == '50':
     print("Obtained elevation value is correct")
 
-print("Testing is successful for Happy path")
+print("Reached end of testing when minimum elevation gain is selected ")
+
+# Testing Happy path when max elevation is provided
+
+driver.refresh()
+
+# find source element and pass the source address as keys
+source_box = driver.find_element("id", "source")
+source_box.send_keys("Worcester,Amherst,MA")
+
+# find destination element and pass the destination address as keys
+destination_box = driver.find_element("id", "destination")
+destination_box.send_keys("university health services, amherst, ma")
+
+# find maximum elevation button and click it
+max_button = driver.find_element("name", "max")
+max_button.click()
+
+# find slider element and slide it
+range_max = driver.find_element("id", "slider")
+actions = ActionChains(driver)
+actions.click_and_hold(range_max).move_by_offset(100, 100)
+actions.perform()
+
+# find Submit button and click it
+submit = driver.find_element("id", "submit button")
+submit.click()
+
+time.sleep(10)
+# find distance statistics and verify the calculated values
+distance = driver.find_element("name", "fee_distance")
+distance_value_max = distance.get_attribute("value")
+if distance_value_max == '3695.6650000000013':
+    print("Obtained distance value is correct for maximum elevation gain")
+
+# find elevation statistics and verify the calculated values
+elevation = driver.find_element("name", "fee_elevation")
+elevation_value_max = elevation.get_attribute("value")
+if elevation_value_max == '48':
+    print("Obtained elevation value is correct for maximum elevation gain")
+
+print("Reached end of testsing when maximum elevation gain is selected")
 
